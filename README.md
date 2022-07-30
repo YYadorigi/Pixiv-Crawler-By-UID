5cc782ff56dc4509f57d25ba8a9b289d416ba743
leyancui <1273425757@qq.com>
2022-07-30 20:01:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Pixiv UID Crawler
+
+一个简单的Pixiv插画爬虫。根据画师的uid，爬取其主页上的全部作品。
+
+*（仅供学习交流用途，请勿传播图片、尝试 ~~[数据删除]~~ 等不适宜行为，违者自负。）*
+
+
+
+# 安装
+
+运行以下命令以创建适配的虚拟环境：
+
+```
+conda create -n <env_name> python=3.8
+```
+
+
+
+运行以下命令安装依赖：
+
+```
+pip install -r requirements.txt
+```
+
+
+
+# 配置
+
+请将仓库中的 `template_settings.json` 改名为 `pixiv_settings.json`，
+
+然后依照实际情况如下填写 `pixiv_settings.json` 文件：
+
+- 必须自行填写的项为：
+
+  `cookie`，`user-agent`，`http`，`https`，`uid`；
+
+- 可以自行修改的项为：
+
+  `interval_between_user`：每爬取不同uid之间的时间间隔（单位：秒），
+
+  `interval_between_download`：每次下载之间的时间间隔（单位：秒）；
+
+  *（警告：不建议将后者设置得过短，过于频繁的访问存在封IP的风险。）*
+
+- 不可修改的项为：
+
+  `referer`：pixiv 主址。
+
+```
+{
+    "headers": {
+        "cookie": "你的COOKIE",
+        "user-agent": "你所需使用的用户代理",
+        "referer": "https://www.pixiv.net/"
+    },
+    "proxies": {
+        "http": "你的http代理",
+        "https": "你的https代理"
+    },
+    "config": {
+        "interval_between_user": 100,
+        "interval_between_download": 2
+    },
+    "uid": [
+        画师的uid，
+        不同uid之间换行并用逗号隔开，
+        最后一行后不加逗号
+    ]
+}
+```
+
+
+
+# 文件说明
+
+- `pixiv_crawler.py`：主程序。执行该程序以运行本爬虫。
+- `pixiv_settings.json`：配置文件。
+- `pixiv_crawler.log`：运行主程序后**自动生成**的日志文件，记录日志输出。
+- `pixiv_illusions`：运行主程序后**自动生成**的目录，按照作者 ID 分目录存放图片。
+- `requirements.txt`：虚拟环境的依赖包。
+
+
+
+# 使用
+
+1. 配置好 `pixiv_settings.json` 后，直接在虚拟环境下运行 `pixiv_crawler.py` 即可。
+2. 运行过程中，程序会自动在本目录下创建 `pixiv_illlusions` 目录，并在该目录下对每个作者的 ID 新建一个子目录，以存放该作者的所有作品。
+3. 终端和日志文件中会实时跟进爬虫运行情况。用户可在 `pixiv_crawler.log` 中查看详情。
---
+++ pixiv_crawler.py
@@ -0,0 +1,150 @@
+import logging
+import requests
+from bs4 import BeautifulSoup as BS
+import json
+import time
+import pathlib as pl
+
+# Set the outputs of log and terminal
+fmt = '%(asctime)s.%(msecs)03d [%(levelname)s] %(message)s'
+datefmt = '%Y-%m-%d %H:%M:%S'
+level = logging.INFO
+
+formatter = logging.Formatter(fmt, datefmt)
+logger = logging.getLogger()
+logger.setLevel(level)
+
+file = logging.FileHandler("./pixiv_crawler.log", encoding='utf-8')
+file.setLevel(level)
+file.setFormatter(formatter)
+logger.addHandler(file)
+
+console = logging.StreamHandler()
+console.setLevel(level)
+console.setFormatter(formatter)
+logger.addHandler(console)
+
+class PixivCrawler:
+    def __init__(self):
+        """
+        Initialize the crawler settings
+
+        :return:
+        """
+        with open("pixiv_settings.json", "r", encoding="utf8") as f:
+            self.settings = json.load(f)    # Load settings
+        logger.info("Settings loaded")
+
+    def sleep(self, sleep_key: str, delta=0):
+        """
+        Execute sleeping for a time configured in the settings
+
+        :param sleep_key: the sleep time label
+        :param delta: added to the sleep time
+        :return:
+        """
+        _t = self.settings["config"][sleep_key] + delta
+        logger.info(f"Sleep {_t} second(s)")
+        time.sleep(_t)
+        
+    def get_pid_lists_by_uid(self, uid: int) -> list:
+        """
+        Get the user's all works by user id
+
+        :param uid: user id
+        :return: pid lists of the user
+        """
+        # Send request
+        url = f"https://www.pixiv.net/ajax/user/{uid}/profile/all?lang=zh"
+        resp = requests.get(url, headers=self.settings["headers"], proxies=self.settings["proxies"])
+
+        # Get pid lists of all works
+        pid_lists = []
+        soup = BS(resp.text, 'lxml').get_text()
+        soup = json.loads(soup)
+        pid_dicts = soup["body"]["illusts"]
+        for key in pid_dicts.keys():
+            pid_lists.append(key)
+        return pid_lists
+
+    def get_links_by_pid(self, pid) -> list:
+        """
+        Get the download links of works by pid
+
+        :param pid: pid of the works
+        :return:
+        """
+        # Send request
+        url = f"https://www.pixiv.net/ajax/illust/{pid}/pages?lang=zh"
+        resp = requests.get(url, headers=self.settings["headers"], proxies=self.settings["proxies"])
+
+        # Get download links
+        links = []
+        soup = BS(resp.text, 'lxml').get_text()
+        soup = json.loads(soup)
+        works = soup["body"]
+        for work in works:
+            links.append(work["urls"]["original"])
+        return links
+        
+    def download(self, url: str, filename: str):
+        """
+        Download the image by url
+
+        :param url: url of the image
+        :return:
+        """
+        resp = requests.get(url, headers=self.settings["headers"], proxies=self.settings["proxies"])
+        with open(filename, "wb") as f:
+            f.write(resp.content)
+        logger.info(f"Download {url.split('/')[-1]}")
+
+    def download_all_works_by_uid(self, uid: int):
+        """
+        Download all works by user id
+
+        :param uid: user id
+        :return:
+        """
+        # Get the user's name
+        resp = requests.get(f"https://www.pixiv.net/ajax/user/{uid}/profile/top?lang=zh", headers=self.settings["headers"], proxies=self.settings["proxies"])
+        soup = BS(resp.text, 'lxml').get_text()
+        soup = json.loads(soup)
+        name = soup["body"]["extraData"]["meta"]["ogp"]["title"]
+        
+        # Make directory
+        dir = pl.Path() / f"pixiv_illusions/{name}"
+        if not dir.exists():
+            dir.mkdir(parents=True)
+            logger.info(f"Make directory {dir}")
+        else:
+            logger.info(f"Directory {dir} already exists")
+
+        # Save all download links of works of the user
+        links = []
+
+        # Get all download links
+        pid_lists = self.get_pid_lists_by_uid(uid)
+        for pid in pid_lists:
+            links += self.get_links_by_pid(pid)
+            logger.info(f"Get {len(links)} links as of pid {pid}")
+
+        # Download all works by the links
+        for link in links:
+            try:
+                self.download(link, f"{dir}/{link.split('/')[-1]}")
+            except Exception as e:
+                logger.error(f"Error: {e}")
+                continue
+            self.sleep("interval_between_download")
+
+if __name__ == "__main__":
+    p = PixivCrawler()
+    for uid in p.settings["uid"]:
+        p.download_all_works_by_uid(uid)
+        p.sleep("interval_between_user")
+
+
+        
+
+
